e4b614ad418580a88e84fdb07cbddef50d87a6aa
Bozhidar Topchiyski <bozhidar.topchiyski@sap.com>
2024-04-08 20:00:41 +0300

    Initial backend implementation

---
+++ backend/src/main/java/com/shop/OnlineShopTuApplication.java
@@ -0,0 +1,13 @@
+package com.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopTuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopTuApplication.class, args);
+	}
+
+}
